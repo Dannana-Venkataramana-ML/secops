8a1059f9a5c88a69c9883acf398aa87fdd9493fe
Dannana-Venkataramana-ML <dannana.venkataramana@maplelabs.com>
2023-03-16 22:11:38 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from flask import render_template_string
+
+app = Flask(__name__)
+app.secret_key = "TmV2ZXIgR29ubmEgR2l2ZSBZb3UgVXA="
+
+@app.route('/')
+def index():
+    name = request.args.get('name') or "Kenobi"
+    template = f"<h1>- Hello There!<br>- General {name}!</h1>"
+    return render_template_string(template)
+
+if __name__ =='__main__':
+    app.run()
